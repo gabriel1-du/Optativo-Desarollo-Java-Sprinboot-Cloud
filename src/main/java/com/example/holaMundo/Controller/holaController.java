a64771161623c018e08e79e9bfb0bb8bf670623d
Gabriel Duran <gabrieldurans999@gmail.com>
2026-08-31 21:35:12 -0400

    nuevas lineas de mensaje

--- src/main/java/com/example/holaMundo/Controller/holaController.java
+++ src/main/java/com/example/holaMundo/Controller/holaController.java
@@ -1,5 +1,24 @@
 package com.example.holaMundo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
 public class holaController {
+
+
+    @GetMapping("saludoGet")
+    public String HolaMundo(){
+        return "¡Hola, Mundo Post!";
+    }
+
+
+    @PostMapping
+    public String HolaMundoPost(){
+        return "¡Hola, Mundo! (POST)";
+    }
 
 }
--- src/main/java/com/example/holaMundo/HolaMundoApplication.java
+++ src/main/java/com/example/holaMundo/HolaMundoApplication.java
@@ -8,8 +8,6 @@ public class HolaMundoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolaMundoApplication.class, args);
-
-		
 	}
 
 }
